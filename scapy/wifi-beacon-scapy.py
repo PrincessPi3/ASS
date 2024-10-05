553d637974c1f146d53e194560db2ccd624b1d3d
PrincessPi3 <human.bagel@gmail.com>
2024-10-05 16:02:23 -0600

    updates

--- scapy/wifi-beacon-scapy.py
+++ scapy/wifi-beacon-scapy.py
@@ -3,10 +3,10 @@ from scapy.all import *
 # https://yasoob.me/2018/09/08/sending-sniffing-wlan-beacon-frames-using-scapy/
 
 iface = 'wlan1'
-sender = 'c2:13:37:c2:13:37'
+sender = RandMAC()
 
 def beacon_raw(SSID, length=255):
-	dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff', addr2=sender, addr3=sender)
+	dot11 = Dot11(type=0, subtype=8, addr1='ff:ff:ff:ff:ff:ff', addr2=RandMAC(), addr3=RandMAC())
 	beacon = Dot11Beacon()
 	essid = Dot11Elt(ID='SSID',info=RawVal(SSID), len=length)
 	frame = RadioTap()/dot11/beacon/essid
@@ -29,6 +29,7 @@ def beacon_normie(SSID):
 #SSID =  b"YOU-SEEM-FUNDAMETALLY-FUN\x00\x0AI-THINK-ID-LIKE-TO-KNOW-YOU\x00\x0AI-FEEL-LIKE-BEING-YOUR-FRIEND\x00\x0AI-AM-YOUR-PONY-WAIFU\x0a\x00\xc6\x54\x00"
 ##SSID ="your-pony-waifu-desu~-desu~-desu~-\xF0\x3D\xF0\x3D\xF0\x3D\x00\x00\x21\xF0\x3D\xF0\x3D\xF0\x3D"
 mostImportantPart = b'pony-waifu-loves-you-too-have-some-binary='
-binaryFun = 0b101010 # binary underflow exploit 100% original oc donut steal
-SSID = binaryFun
+binaryFun = 0b0 # binary underflow exploit 100% original oc donut steal
+# SSID = binaryFun
+SSID = "\0\0\0\0\0\0\0\0\0\0\0\0"
 beacon_raw(SSID, length=255)
